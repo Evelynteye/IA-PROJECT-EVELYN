1b6b836b90142bfb780db3c38ee36c153490e06f
Evelynteye <91224184+Evelynteye@users.noreply.github.com>
2021-11-17 10:07:23 -1200

    Update App.js

--- IA_PROJECT/App.js
+++ IA_PROJECT/App.js
@@ -1,32 +1,24 @@
-// import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
-// import { createNativeStackNavigator } from 'react-navigation-stack';
 
-// createStackNavigator(RouteConfigs, StackNavigatorConfig);
-// creating an instance of the stack navigator
-
-
-// Custom imports
-import HomeScreen from './source/view/screens/HomeScreen';
-import DetailsSceeen from './source/view/screens/DetailsScreen';
-import LoginScreen from './source/view/screens/LoginScreen';
+import Home from './src/view/screens/Home';
+import Details from './src/view/screens/Details';
+import Login from './src/view/screens/Login';
 import {StatusBar} from 'react-native';
-import COLORS from './source/variables/colors';
+import COLORS from './src/customs/colors';
 
-// Using arrow function to describe the main App component
+
 const App = () => {
-  // const Stack = createNativeStackNavigator();
   const Stack = createStackNavigator();
   return (
     <NavigationContainer>
       <StatusBar barStyle="dark-content" backgroundColor={COLORS.white} />
       <Stack.Navigator screenOptions={{header: () => null}}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsSceeen} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
     </NavigationContainer>
   );
