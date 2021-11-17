96f69f7379de6bef2dd063ed256e46e042299ecd
Evelynteye <91224184+Evelynteye@users.noreply.github.com>
2021-11-17 10:08:33 -1200

    Create products.js

---
+++ IA_PROJECT/src/customs/products.js
@@ -0,0 +1,60 @@
+const products = [
+  {
+    id: 1,
+    name: 'Winter Coats',
+    price: '39.99',
+    like: true,
+    img: "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
+    about:
+      'Note:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester; Detachable Liner Fabric: Warm Fleece. Detachable Functional Liner: Skin Friendly, Lightweigt and Warm.Stand Collar Liner jacket, keep you warm in cold weather. Zippered Pockets: 2 Zippered Hand Pockets, 2 Zippered Pockets on Chest (enough to keep cards or keys)and 1 Hidden Pocket Inside.Zippered Hand Pockets and Hidden Pocket keep your things secure. Humanized Design: Adjustable and Detachable Hood and Adjustable cuff to prevent the wind and water,for a comfortable fit. 3 in 1 Detachable Design provide more convenience, you can separate the coat and inner as needed, or wear it together. It is suitable for different season and help you adapt to different climates',
+  },
+
+  {
+    id: 2,
+    name: 'Biker Jacket',
+    price: '29.99',
+    like: false,
+    img: "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg",
+    about:
+      '100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON',
+  },
+  {
+    id: 3,
+    name: 'Boat Neck V',
+    price: '25.99',
+    like: false,
+    img: "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg",
+    about:
+      '95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem',
+  },
+
+  {
+    id: 4,
+    name: 'Short Sleeve',
+    price: '25.99',
+    like: true,
+    img: "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
+    about:
+      '100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away, Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort',
+  },
+  {
+    id: 5,
+    name: 'Cotton Short',
+    price: '50.99',
+    like: true,
+    img: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
+    about:
+      '95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.',
+  },
+  {
+    id: 6,
+    name: 'Women\'s Short',
+    price: '50.99',
+    like: false,
+    img: "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
+    about:
+      '100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away, Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort',
+  },
+];
+
+export default products;
